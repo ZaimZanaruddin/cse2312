12a6b3c8a376b81ba035f6fb2a40bef664424518
Zaim Zanaruddin <zaimzanaruddin@Zaims-MacBook-Pro.local>
2016-12-09 19:44:10 -0600

    test

--- finalproject.s
+++ finalproject.s
@@ -24,8 +24,7 @@ main:
     BL _getchar
     MOV R9,R0
     BL _getop
-    VMOV S2,S1
-    VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
+    VCVT.F64.F32 D4, S1     @ covert the result to double precision for printing
     VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
     BL  _printf_result      @ print the result
 
